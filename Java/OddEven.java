8cef9094815e8755219d2f45ad507a2fcb0a3cc7
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-28 16:56:25 +0800

    Create OddEven.java

---
+++ Java/OddEven.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class OddEven{
+  
+  
+  //Drivers Program
+ public static void main(String[] args){
+  
+   Scanner in = new Scanner(System.in);
+  
+   //Storage input
+   int num;
+   
+   System.out.print("Enter a number: ");
+   num = in.nextInt();
+   
+   //condition
+   if(num%2 == 0){
+    System.out.println("Inputed Number: " + num + "\n Is Even Number.");
+   }
+   else System.out.println("Inputed Number: " + num  + "\n Is Odd Number.");
+ }
+  
+}
